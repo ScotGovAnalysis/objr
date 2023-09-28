888d6b92dc912e0284f7fd5fc92fc33783914bd4
alicebyers5 <alice.byers@gov.scot>
2023-09-28 14:08:01 +0100

    Remove token from env after tests

--- tests/testthat/test-store_token.R
+++ tests/testthat/test-store_token.R
@@ -10,13 +10,15 @@ test_that("Error if Authorization header doesn't exist", {
 test_that("Function returns invisible object", {
   expect_invisible(
     httr2::response(headers = list(Authorization = "test1")) |>
-      store_token()
+      store_token(store_env = environment())
   )
+  rm(test, envir = environment())
 })
 
 test_that("Environment value created successfully", {
   httr2::response(headers = list(Authorization = "test2")) |>
-    store_token()
+    store_token(store_env = environment())
   expect_true(exists("token"))
   expect_equal(get("token"), "test2")
+  rm(test, envir = environment())
 })
