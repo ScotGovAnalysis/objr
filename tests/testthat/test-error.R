d788979dc84bbbcf81ae3cc8c731cf91ddd04a24
alicebyers5 <alice.byers@gov.scot>
2024-04-09 14:17:19 +0100

    Add test for error fn

--- tests/testthat/test-error.R
+++ tests/testthat/test-error.R
@@ -1,8 +1,15 @@
 test_that("Expect character value returned", {
+
   expect_type(
     error(httr2::response(status_code = 401)),
     "character"
   )
+
+  expect_type(
+    error(httr2::response(status_code = 403)),
+    "character"
+  )
+
 })
 
 test_that("NULL returned for invalid status code", {
