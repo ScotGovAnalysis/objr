c3012f2d6294cf27e3828e83d63dc352fedbf08b
alicebyers5 <alice.byers@gov.scot>
2024-05-20 16:37:10 +0100

    Add function to check pages returned

--- tests/testthat/test-objectiveR.R
+++ tests/testthat/test-objectiveR.R
@@ -1,3 +1,5 @@
+# objectiveR ----
+
 without_internet({
 
   test_that("Valid request created", {
@@ -38,5 +40,89 @@ with_mock_api({
       })
 
     })
+
+})
+
+
+# error ----
+
+test_that("Expect character value returned", {
+
+  expect_type(
+    error(httr2::response(status_code = 401)),
+    "character"
+  )
+
+  expect_type(
+    error(httr2::response(status_code = 403)),
+    "character"
+  )
+
+})
+
+test_that("NULL returned for invalid status code", {
+  expect_null(
+    error(httr2::response(status_code = 100))
+  )
+})
+
+
+# check_pages ----
+
+test_that("NULL returned", {
+
+  expect_null(
+    check_pages(httr2::response_json())
+  )
+
+  expect_null(
+    check_pages(httr2::response_json(
+      body = list(content = "test_content")
+    ))
+  )
+
+  resp1 <- httr2::response_json(
+    body = list(metadata = list(
+      totalPages = 1,
+      page = 0
+    ))
+  )
+
+  expect_null(check_pages(resp1))
+
+})
+
+test_that("Error returned", {
+
+  resp1 <- httr2::response_json(
+    body = list(metadata = list(
+      totalPages = 2
+    ))
+  )
+
+  expect_error(check_pages(resp1), class = "metadata-values-dont-exist")
+
+  resp2 <- httr2::response_json(
+    body = list(metadata = list(
+      totalPages = 2,
+      page = 4
+    ))
+  )
+
+  expect_error(check_pages(resp2), class = "page-doesnt-exist")
+
+})
+
+test_that("Warning and message returned", {
+
+  resp <- httr2::response_json(
+    body = list(metadata = list(
+      totalPages = 2,
+      page = 0
+    ))
+  )
+
+  expect_warning(suppressMessages(check_pages(resp)))
+  expect_message(suppressWarnings(check_pages(resp)))
 
 })
