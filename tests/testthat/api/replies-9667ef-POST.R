c08b5e79ad9e9c6ee43952e3137e330ec6d8b4d2
alice-hannah <alice.hannah@gov.scot>
2024-08-12 14:43:30 +0100

    Test comment functions

---
+++ tests/testthat/api/replies-9667ef-POST.R
@@ -0,0 +1,13 @@
+structure(list(method = "POST", url = "https://api/replies",
+    status_code = 200L, headers = structure(list(Date = "Mon, 12 Aug 2024 13:17:30 GMT",
+        `Content-Type` = "application/json", `Transfer-Encoding` = "chunked",
+        Connection = "keep-alive", `X-CONNECT-MDC` = "apisnEiGPzU",
+        `X-Frame-Options` = "deny", `X-XSS-Protection` = "1; mode=block",
+        `Cache-Control` = "no-cache, no-store", Expires = "0",
+        Pragma = "no-cache", `Strict-Transport-Security` = "max-age=31536000 ; includeSubDomains",
+        `Content-Security-Policy` = "script-src 'self' 'unsafe-inline'",
+        `X-Content-Type-Options` = "nosniff", `Referrer-Policy` = "strict-origin-when-cross-origin",
+        `Feature-Policy` = "vibrate 'none'; geolocation 'none'",
+        Authorization = "REDACTED", `Set-Cookie` = "REDACTED"), class = "httr2_headers"),
+    body = charToRaw("{\"model\":\"Comment\",\"uuid\":\"test_uuid\"}"),
+    cache = new.env(parent = emptyenv())), class = "httr2_response")
