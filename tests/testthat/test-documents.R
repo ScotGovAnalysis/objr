27905e61f35d190315926e8940859b7e4b59e365
alice-hannah <alice.hannah@gov.scot>
2024-10-08 18:08:04 +0100

    Test rollback_to_version()

--- tests/testthat/test-documents.R
+++ tests/testthat/test-documents.R
@@ -26,3 +26,42 @@ with_mock_api({
   })
 
 })
+
+
+# rollback_to_version ----
+
+without_internet({
+
+  test_that("Valid request", {
+
+    expect_PUT(
+      rollback_to_version(document_uuid = "test_document_uuid",
+                          version_uuid = "test_version_uuid"),
+      paste0("https://secure.objectiveconnect.co.uk/publicapi/1/",
+             "documents/test_document_uuid/rollback"),
+      "{\"targetVersionUuid\":\"test_version_uuid\"}"
+    )
+
+  })
+
+})
+
+with_mock_api({
+
+  test_that("Function returns invisible", {
+
+    expect_invisible(
+      suppressMessages(rollback_to_version(document_uuid = "test_document",
+                                           version_uuid = "test_version"))
+    )
+
+  })
+
+  test_that("Function returns success message", {
+
+    expect_message(rollback_to_version(document_uuid = "test_document",
+                                       version_uuid = "test_version"))
+
+  })
+
+})
