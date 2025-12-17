14aa99c1e3b2e2f0547ad39cd09f36d63c4b0ab4
alice-hannah <alice.hannah@gov.scot>
2025-12-17 12:13:55 +0000

    Don't lint long URLs

--- R/objr-package.R
+++ R/objr-package.R
@@ -1,9 +1,11 @@
 #' @keywords internal
 #' @references
+# nolint start: line_length_linter
 #' * [\{objr\} package website](https://ScotGovAnalysis.github.io/objr/)
 #' * [API authentication](https://scotgovanalysis.github.io/objr/articles/authentication.html)
 #' * [Two-factor authentication](https://scotgovanalysis.github.io/objr/articles/two-factor.html)
 #' * [Objective Connect API documentation](https://secure.objectiveconnect.co.uk/publicapi/1/swagger-ui/index.html?configUrl=/publicapi/1/v3/api-docs/swagger-config#/)
+# nolint end
 #'
 "_PACKAGE"
 
