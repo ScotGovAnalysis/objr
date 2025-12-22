be6139c5bbce4518c9b6c99d71ba739f6eae3180
alice-hannah <alice.hannah@gov.scot>
2025-12-22 16:42:57 +0000

    Fix URL to interactive docs

--- R/bypass_2fa.R
+++ R/bypass_2fa.R
@@ -96,7 +96,7 @@ participant_bypass_2fa <- function(participant_uuid,
 #'
 #' More details on this endpoint are available in the
 # nolint start: line_length_linter
-#' \href{https://secure.objectiveconnect.co.uk/publicapi/1/swagger-ui/index.html?configUrl=/publicapi/1/v3/api-docs/swagger-config#/Workgroups/setTwoStepMandatory}{API documentation}.
+#' \href{https://secure.objectiveconnect.co.uk/publicapi/1/swagger-ui/index.html#/Workgroups/setTwoStepMandatory}{API documentation}.
 # nolint end
 #'
 #' @param workgroup_uuid Workgroup UUID
